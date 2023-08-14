fc305af0faaaeff689ea50d2d15dfe8799ebb26e
Esteban AA <62820991+eathekilla@users.noreply.github.com>
2023-08-14 12:45:03 -0500

    Update main.py

--- project/main.py
+++ project/main.py
@@ -8,13 +8,13 @@ from pydantic import BaseModel
 from .worker import create_task,transfer
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="project/static"), name="static")
-templates = Jinja2Templates(directory="templates")
+""" app.mount("/static", StaticFiles(directory="project/static"), name="static")
+templates = Jinja2Templates(directory="templates") """
 
 
 @app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("home.html", context={"request": request})
+async def get_status():
+    return {"status": "OK"}
 
 
 @app.post("/tasks", status_code=201)
